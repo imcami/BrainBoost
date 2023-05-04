b81822acdb5e69f453fe4d40bd766429af20407b
Camila Lo Nero <lonerocamila@gmail.com>
2023-05-04 18:48:58 -0300

    landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar/Navbar'
-
+import Error404 from './components/404/Error404';
+import Landing from './layouts/Landing/Landing'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-
-    <div classNameName="App">
-      <NavBar/>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} classNameName="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} classNameName="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div classNameName="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p classNameName="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+     
+    <BrowserRouter>
+    <div className='app'>
+       <NavBar/>
+        <Routes>  
+          <Route exact path="/" element={<Landing/>} />
+          <Route exact path='*' element={<Error404/>} />
+        </Routes> 
+        </div>
+      </BrowserRouter>
   )
 }
 
